89d43f21d9420bcf25fc933235e7e49497afeb9a
Shane Emmons <semmons99@gmail.com>
2012-03-06 14:44:09 -0500

    refactor

--- lib/anita/bot.rb
+++ lib/anita/bot.rb
@@ -26,12 +26,24 @@ module Anita
       @plugins ||= {}
     end
 
+    def assimilated?(name)
+      plugins.keys.include?(name)
+    end
+
+    def usage(name)
+      plugins[name].usage
+    end
+
+    def plugin_names
+      plugins.keys
+    end
+
     private
 
     def setup_basic_responder
       bot.on(:message) do |m|
         message = Anita::Message.new(m)
-        Anita::Bot.plugins.each_value do |plugin|
+        Anita::Bot.plugins.each do |name, plugin|
           response = plugin.process(message)
           m.reply(response) unless response.nil? || response == ""
         end
@@ -41,23 +53,22 @@ module Anita
     def setup_usage_responder
       bot.on(:message, /^#{bot.nick} (usage|help)$/) do |m|
         Anita::Bot.plugins.each do |name, plugin|
-          m.reply "#{name}: #{plugin.usage}"
+          m.reply("#{name}: #{plugin.usage}")
         end
       end
 
       bot.on(:message, /^#{bot.nick} (usage|help) (.+)$/) do |m, _, name|
-        resp = if Anita::Bot.plugins.include?(name)
-                 Anita::Bot.plugins[name].usage
-               else
-                 "unknown plugin"
-               end
-        m.reply "#{name}: #{resp}"
+        if Anita::Bot.assimilated?(name)
+          m.reply(Anita::Bot.usage(name))
+        else
+          "unknown plugin"
+        end
       end
     end
 
     def setup_plugins_responder
       bot.on(:message, /^#{bot.nick} plugins$/) do |m|
-        m.reply Anita::Bot.plugins.keys.join(", ")
+        m.reply(Anita::Bot.plugin_names.join(", "))
       end
     end
   end
